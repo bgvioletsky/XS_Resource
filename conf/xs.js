c3aee396d6c81de488a63f8b9134e6b261518776
bgcode <violetcode@icloud.com>
2024-10-12 21:30:09 +0800

    update

--- conf/xs.js
+++ conf/xs.js
@@ -1,6 +1,6 @@
 const bg = new Env('xs');
 
-bg.version = '0.1.3';
+bg.version = '0.1.4';
 bg.json = bg.name // `接口`类请求的响应体
 bg.html = bg.name // `页面`类请求的响应体
 // bg.url = "http://192.168.1.78:8080/index.html";
